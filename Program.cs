0f1a42ae6e2e2222bad5a601d4556f27ab6b6493
Dom118 <dominic.chambers@cognizant.com>
2025-03-27 15:02:52 +0000

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -10,6 +10,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddDbContext<AppDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
+builder.Services.AddHttpClient();
 // Add services to the container.
 builder.Services.AddControllers();
 
